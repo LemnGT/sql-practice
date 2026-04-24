3c4227dc4749d51ef963a1f191a3d3c123043049
Marcelo G <m2biel000@icloud.com>
2026-04-24 17:54:55 +0100

    Enhance SQL file with problem statement and goal
    Added problem statement and goal comments for clarity.

--- basic-joins /Product_Sales_Analysis_I.sql
+++ basic-joins /Product_Sales_Analysis_I.sql
@@ -1,4 +1,8 @@
--- Problem: Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+-- Problem: Product Sales Analysis I
+--
+-- Goal:
+-- report the product_name, year, and price for each sale_id in the Sales table.
+
 
 SELECT p.product_name, s.year, s.price 
 FROM Sales as s
