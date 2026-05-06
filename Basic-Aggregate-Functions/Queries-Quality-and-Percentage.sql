087dcb29c5dc729d47380ecb0b1b8aac85a14026
Marcelo G <m2biel000@icloud.com>
2026-05-07 00:32:48 +0100

    Queries Quality and Percentage

---
+++ Basic-Aggregate-Functions/Queries-Quality-and-Percentage.sql
@@ -0,0 +1,39 @@
+-- Problem: Queries Quality and Percentage
+--
+-- Goal: Write a solution to find each query_name, the quality and poor_query_percentage.
+--       Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+/*
+Table: Queries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| query_name  | varchar |
+| result      | varchar |
+| position    | int     |
+| rating      | int     |
++-------------+---------+
+This table may have duplicate rows.
+This table contains information collected from some queries on a database.
+The position column has a value from 1 to 500.
+The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
+ 
+We define query quality as:
+The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+The percentage of all queries with rating less than 3.
+*/
+--Using AVG
+SELECT query_name, ROUND(AVG(rating / position), 2) AS quality, ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
+
+--Using SUM, CASE and COUNT
+SELECT 
+  query_name, 
+  ROUND(SUM(rating/position) / COUNT(rating),2) AS quality, 
+  ROUND(SUM(CASE WHEN rating <3 THEN 1 ELSE 0 END)/COUNT(rating)*100 ,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
